6623366b82c0b046db130aeea6e68e3dcbff7e66
Rizal M <rizalmadani70@gmail.com>
2023-08-11 15:16:50 +0700

    Buat middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'pelanggan' => \App\Http\Middleware\isPelanggan::class,
+        'admin' => \App\Http\Middleware\isAdmin::class,
     ];
 }
