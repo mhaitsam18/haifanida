8575353583bddf6692014d686792836a3455afb0
Rizal M <rizalmadani70@gmail.com>
2023-08-18 10:33:35 +0700

    Update unit test

--- tests/Feature/FirstIterationTest.php
+++ tests/Feature/FirstIterationTest.php
@@ -12,6 +12,18 @@ class FirstIterationTest extends TestCase
 {
     // use RefreshDatabase;
 
+    public function test_routing_halaman_daftar_paket_sukses() : void {
+        $request = $this->get('/paket');
+
+        $request->assertSuccessful();
+    }
+
+    public function test_respon_mengembalikan_halaman_daftar_paket() : void {
+        $request = $this->get('/paket');
+
+        $request->assertViewIs('landing-page.daftar-paket');
+    }
+
     public function test_terdapat_data_paket_yang_sama_seperti_di_database(): void
     {
         $response = $this->get('/paket');
@@ -22,6 +34,24 @@ class FirstIterationTest extends TestCase
         });
 
         $response->assertSee($paketFlatArray);
+    }
+
+    public function test_routing_halaman_detail_paket() : void {
+        $request = $this->get('/paket/1');
+
+        $request->assertSuccessful();
+    }
+
+    public function test_respon_mengembalikan_halaman_detail_paket() : void {
+        $request = $this->get('/paket/1');
+
+        $request->assertViewIs('landing-page.paket');
+    }
+
+    public function test_routing_halaman_detail_paket_sembarang() : void {
+        $request = $this->get('/paket/1000');
+
+        $request->assertNotFound();
     }
 
     public function test_terdapat_data_paket_yang_dimaksud(): void
@@ -37,21 +67,59 @@ class FirstIterationTest extends TestCase
         $response->assertSee($paketFlatArray);
     }
 
+    public function test_autentikasi_route_pemesanan() : void {
+        $request = $this->get('/pesan/1');
+
+        $request->assertRedirect('/login');
+    }
+
+    public function test_routing_halaman_pemesanan_sukses() : void {
+        $pelanggan = User::where('role', 'pelanggan')->first();
+
+        $request = $this->actingAs($pelanggan)
+            ->get('/pesan/1');
+
+        $request->assertSuccessful();
+    }
+
+    public function test_respon_mengembalikan_halaman_pemesanan() : void {
+        $pelanggan = User::where('role', 'pelanggan')->first();
+
+        $request = $this->actingAs($pelanggan)
+            ->get('/pesan/1');
+
+        $request->assertViewIs('landing-page.pesan-paket');
+    }
+
     public function test_menghasilkan_pesanan_baru() : void {
         $pelanggan = User::where('role', 'pelanggan')->first();
 
         $this->actingAs($pelanggan)
             ->post('/pesan', [
-            'paket' => 1,
-            'jenis-kamar' => 'single',
-            'jemaah' => ['Jemaah Test 1'],
-            'jenis-kelamin' => ['laki-laki'],
-        ]);
+                'paket' => 1,
+                'jenis-kamar' => 'single',
+                'jemaah' => ['Jemaah Test 1'],
+                'jenis-kelamin' => ['laki-laki'],
+            ]);
 
         $dataBaru = Jemaah::latest()->first()->toArray();
 
         // $this->assertContains(['jenis_kelamin' => 'laki-laki', 'nama' => 'Jemaah Test 1'], $dataBaru);
         $this->assertContains('Jemaah Test 1', $dataBaru);
+    }
+
+        public function test_menerima_request_input() : void {
+        $pelanggan = User::where('role', 'pelanggan')->first();
+
+        $request = $this->actingAs($pelanggan)
+            ->post('/pesan', [
+                'paket' => 1,
+                'jenis-kamar' => 'single',
+                'jemaah' => ['Jemaah Test 10'],
+                'jenis-kelamin' => ['laki-laki'],
+            ]);
+
+        $request->assertStatus(302);
     }
 
     public function test_stok_berkurang() : void {
@@ -62,18 +130,35 @@ class FirstIterationTest extends TestCase
 
         $this->actingAs($pelanggan)
             ->post('/pesan', [
-            'paket' => $id,
-            'jenis-kamar' => 'couple',
-            'jemaah' => ['Jemaah Test 2', 'Jemaah Test 3'],
-            'jenis-kelamin' => ['laki-laki', 'perempuan'],
-        ]);
+                'paket' => $id,
+                'jenis-kamar' => 'couple',
+                'jemaah' => ['Jemaah Test 2', 'Jemaah Test 3'],
+                'jenis-kelamin' => ['laki-laki', 'perempuan'],
+            ]);
 
         $paketSesudah = Paket::select('stok')->where('id', $id)->first();
 
         $this->assertEquals($paketSesudah->stok, $paketSebelum->stok - 2);
     }
 
-    public function test_data_harga_di_daftar_paket() : void {
+    public function test_menampilkan_pesan_konfirmasi() : void {
+        $id = 1;
+
+        $paketSebelum = Paket::select('stok')->where('id', $id)->first();
+        $pelanggan = User::where('role', 'pelanggan')->first();
+
+        $request = $this->actingAs($pelanggan)
+            ->post('/pesan', [
+                'paket' => $id,
+                'jenis-kamar' => 'single',
+                'jemaah' => ['Jemaah Test 4'],
+                'jenis-kelamin' => ['laki-laki'],
+            ]);
+
+        $request->assertSessionHas('alert');
+    }
+
+    public function test_menampilkan_data_harga_di_daftar_paket() : void {
         $paket = Paket::select('harga_single')->first();
 
         $request = $this->get('/paket');
@@ -81,7 +166,7 @@ class FirstIterationTest extends TestCase
         $request->assertSee(rupiah($paket->harga_single));
     }
 
-    public function test_data_harga_di_paket_yang_sesuai() : void {
+    public function test_menampilkan_data_harga_di_paket_yang_sesuai() : void {
         $id = 1;
 
         $paket = Paket::select('harga_single')->where('id', $id)->first();
@@ -111,7 +196,7 @@ class FirstIterationTest extends TestCase
         $request->assertSee($fasilitas, false);
     }
 
-    public function test_data_hotel_mekah() : void {
+    public function test_menampilkan_data_hotel_mekah() : void {
         $id = 1;
 
         $paket = Paket::where('id', $id)->first();
@@ -120,7 +205,7 @@ class FirstIterationTest extends TestCase
         $request->assertSee($paket->hotelMekah->nama);
     }
 
-    public function test_data_hotel_madinah() : void {
+    public function test_menampilkan_data_hotel_madinah() : void {
         $id = 1;
 
         $paket = Paket::where('id', $id)->first();
@@ -129,7 +214,7 @@ class FirstIterationTest extends TestCase
         $request->assertSee($paket->hotelMadinah->nama);
     }
 
-    public function test_data_jumlah_hari() : void {
+    public function test_menampilkan_data_jumlah_hari() : void {
         $id = 1;
 
         $paket = Paket::select('jumlah_hari')->where('id', $id)->first();
