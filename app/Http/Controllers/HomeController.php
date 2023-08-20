0d37680d72565c32fea9206a61001de2f895aba2
Rizal M <rizalmadani70@gmail.com>
2023-08-20 16:09:47 +0700

    Tambah lihat faq dan testimoni

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Paket;
+use App\Models\Faq;
+use App\Models\Testimoni;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('landing-page.index');
+        return view('landing-page.index', [
+            'testimoni' => Testimoni::all(),
+            'faq' => Faq::all()
+        ]);
     }
 }
--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -8,45 +8,101 @@
 @section('sub-h1', 'Sub Header H1')
 
 @section('content')
-    <section class="module">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
+<section class="module">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="module">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <p>Perspiciatis laudantium explicabo,</p>
+<section class="module bg-dark-60 pt-0 pb-0 parallax-bg testimonial" data-background="assets/images/testimonial_bg.jpg">
+    <div class="testimonials-slider pt-140 pb-140">
+        <ul class="slides">
+            @foreach ($testimoni as $t)
+            <li>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="module-icon"><span class="icon-quote"></span></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2">
+                            <blockquote class="testimonial-text font-alt">
+                                {{ $t->testimoni }}
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4 col-sm-offset-4">
+                            <div class="testimonial-author">
+                                <div class="testimonial-caption font-alt">
+                                    <div class="testimonial-title">{{ $t->pelanggan->user->name }}</div>
+                                    <div class="testimonial-descr">{{ $t->rating }}/5</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+</section>
+
+<section class="module">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+              <h2 class="module-title font-alt">FAQ</h2>
+              <div class="module-subtitle font-serif">Pertanyaan yang sering ditanyakan</div>
+            </div>
+          </div>
+
+
+        <div class="row">
+            <div class="col-12">
+                @foreach ($faq as $f)
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">
+                        <h4 class="panel-title font-alt"><a data-toggle="collapse" data-parent="#accordion" href="{{ "#sales".$loop->iteration }}">{{ $f->pertanyaan }}</a></h4>
+                      </div>
+                      <div class="panel-collapse collapse" id="{{ "sales".$loop->iteration }}">
+                        <div class="panel-body">
+                            {{ $f->jawaban }}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                @endforeach
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
 
 @section('css')
-    <link href="/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/lib/animate.css/animate.css" rel="stylesheet">
-    <link href="/lib/et-line-font/et-line-font.css" rel="stylesheet">
-    <link href="/lib/flexslider/flexslider.css" rel="stylesheet">
-    <link href="/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
-    <link href="/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
-    <link href="/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+<link href="/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="/lib/animate.css/animate.css" rel="stylesheet">
+<link href="/lib/et-line-font/et-line-font.css" rel="stylesheet">
+<link href="/lib/flexslider/flexslider.css" rel="stylesheet">
+<link href="/lib/owl.carousel/dist/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="/lib/owl.carousel/dist/assets/owl.theme.default.min.css" rel="stylesheet">
+<link href="/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+<link href="/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
 @endsection
 
 @section('js')
-    <script src="/lib/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
-    <script src="/lib/isotope/dist/isotope.pkgd.js"></script>
-    <script src="/lib/imagesloaded/imagesloaded.pkgd.js"></script>
-    <script src="/lib/flexslider/jquery.flexslider.js"></script>
-    <script src="/lib/owl.carousel/dist/owl.carousel.min.js"></script>
-    <script src="/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
-    <script src="/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
+<script src="/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
+<script src="/lib/isotope/dist/isotope.pkgd.js"></script>
+<script src="/lib/imagesloaded/imagesloaded.pkgd.js"></script>
+<script src="/lib/flexslider/jquery.flexslider.js"></script>
+<script src="/lib/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
+<script src="/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
 @endsection
