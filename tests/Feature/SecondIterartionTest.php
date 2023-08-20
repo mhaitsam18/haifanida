73dcb3da4c8bac5e2f78bc1e740bc89ac8661b97
Rizal M <rizalmadani70@gmail.com>
2023-08-20 16:09:59 +0700

    Tambah unit test untuk iterasi kedua

---
+++ tests/Feature/SecondIterartionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Jemaah;
+use App\Models\Paket;
+use App\Models\User;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SecondIterationTest extends TestCase
+{
+    // use RefreshDatabase;
+
+    public function test_menyediakan_informasi_persyaratan() : void {
+        $fasilitas = [
+            'Ketentuan pemesanan paket:',
+            'Melakukan pembayaran uang muka sebesar Rp10.0000.0000,-',
+            'Melunasi pembayaran paket paling lambat 30 hari sebelum keberangkatan',
+            'Dokumen yang akan diperlukan:',
+            'KTP',
+            'KK',
+            'Buku Nikah (jika ada)',
+            'Paspor minimal 2 nama dan berlaku minimal 8 bulan dari tanggal kepulangan',
+            'Foto terbaru ukuran 4&times;6, 5 lembar, zoom wajah',
+        ];
+
+        $request = $this->get('/paket/1');
+
+        $request->assertSee($fasilitas, false);
+    }
+
+    public function test_menyediakan_informasi_jadwal() : void {
+
+    }
+
+    public function test_menyediakan_informasi_barang_boleh_dibawah() : void {
+
+    }
+
+    public function test_menyediakan_informasi_barang_boleh_tidak_boleh_dibawah() : void {
+
+    }
+
+    public function test_menyediakan_informasi_testimoni() : void {
+
+    }
+
+    public function test_hanya_menampilkan_testimoni_yang_diberi_label() : void {
+
+    }
+
+    public function test_menyediakan_informasi_faq() : void {
+
+    }
+
+}
