bb2e19dd3e7c6f7f6d7f83d539fd46ba5ba87ac7
Rizal M <rizalmadani70@gmail.com>
2023-08-25 15:26:25 +0700

    Update link beranda dan testimoni

--- resources/views/partials/landing-page/nav.blade.php
+++ resources/views/partials/landing-page/nav.blade.php
@@ -4,7 +4,7 @@
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#custom-collapse"><span
                     class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span
                     class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand"
-                href="index.html">Haifa Nida Wisata</a>
+                href="{{ route('beranda') }}">Haifa Nida Wisata</a>
         </div>
         <div class="collapse navbar-collapse" id="custom-collapse">
             <ul class="nav navbar-nav navbar-right">
