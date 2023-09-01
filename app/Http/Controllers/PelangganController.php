216b4f5da2bce0c617ff483a0b7faa885dfbedad
Rizal M <rizalmadani70@gmail.com>
2023-09-01 11:32:36 +0700

    Tambah dashboard pelanggan

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pesanan;
 use Illuminate\Http\Request;
 
 class PelangganController extends Controller
 {
-    //
+    public function index() {
+        return view('pelanggan.index', [
+            'pesanan' => Pesanan::orderBy('id', 'DESC')->get(),
+        ]);
+    }
 }
--- config/adminlte.php
+++ config/adminlte.php
@@ -301,82 +301,39 @@ return [
             'type' => 'sidebar-menu-search',
             'text' => 'search',
         ],
+
+        // Menu admin:
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'text'  => 'Dashboard',
+            'url'   => 'admin',
+            'icon'  => 'fa fa-home mr-2',
+            'can'   => 'admin'
+        ],
+
+        // Menu pelanggan:
+        [
+            'text'  => 'Dashboard',
+            'url'   => 'pelanggan',
+            'icon'  => 'fa fa-home mr-2',
+            'can'   => 'pelanggan'
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text'  => 'Pesanan',
+            'url'   => 'pelanggan/pesanan',
+            'icon'  => 'fa fa-box mr-2',
+            'can'   => 'pelanggan'
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text'  => 'Testimoni',
+            'url'   => 'pelanggan/testimoni',
+            'icon'  => 'fa fa-comment mr-2',
+            'can'   => 'pelanggan'
         ],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text'  => 'Kontak Admin',
+            'url'   => 'pelanggan/kontak-admin',
+            'icon'  => 'fa fa-id-badge mr-2',
+            'can'   => 'pelanggan'
         ],
     ],
 
