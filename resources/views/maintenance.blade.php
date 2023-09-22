f0804223b12b4ff10919c3a525d6b5ad79378ea6
mhaitsam18 <haitsam03@gmail.com>
2023-09-23 01:42:54 +0700

    maintenance begin

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="zxx">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/animate.min.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/fonts/flaticon.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/boxicons.min.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/owl.carousel.min.css">
+<link rel="stylesheet" href="/assets-techex-demo/css/owl.theme.default.min.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/magnific-popup.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/nice-select.min.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/meanmenu.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/style.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/responsive.css">
+
+<link rel="stylesheet" href="/assets-techex-demo/css/theme-dark.css">
+
+<link rel="icon" type="image/png" href="/assets-techex-demo/images/favicon.png">
+<title>Haifa Nida Wisata</title>
+</head>
+<body>
+
+<div class="preloader">
+<div class="d-table">
+<div class="d-table-cell">
+<div class="spinner"></div>
+</div>
+</div>
+</div>
+
+
+<div class="error-area">
+<div class="d-table">
+<div class="d-table-cell">
+<div class="error-content">
+<img src="/assets/img/logo-lanskap-2.png" alt="Image">
+<h3>Situs ini sedang dalam perbaikan</h3>
+<p>Mohon maaf atas ketidaknyamanannya, Kami akan segera kembali :)</p>
+{{-- <a href="index.html" class="default-btn btn-bg-two">
+Return To Home Page
+</a> --}}
+</div>
+</div>
+</div>
+</div>
+
+
+{{-- <div class="switch-box">
+<label id="switch" class="switch">
+<input type="checkbox" onchange="toggleTheme()" id="slider">
+<span class="slider round"></span>
+</label>
+</div> --}}
+
+
+<script src="/assets-techex-demo/js/jquery.min.js"></script>
+
+<script src="/assets-techex-demo/js/bootstrap.bundle.min.js"></script>
+
+<script src="/assets-techex-demo/js/owl.carousel.min.js"></script>
+
+<script src="/assets-techex-demo/js/jquery.magnific-popup.min.js"></script>
+
+<script src="/assets-techex-demo/js/jquery.nice-select.min.js"></script>
+
+<script src="/assets-techex-demo/js/wow.min.js"></script>
+
+<script src="/assets-techex-demo/js/meanmenu.js"></script>
+
+<script src="/assets-techex-demo/js/jquery.ajaxchimp.min.js"></script>
+
+<script src="/assets-techex-demo/js/form-validator.min.js"></script>
+
+<script src="/assets-techex-demo/js/contact-form-script.js"></script>
+
+<script src="/assets-techex-demo/js/custom.js"></script>
+</body>
+</html>
