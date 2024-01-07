bdf44c317f09fa9db9ff84a4814174fd20f38c99
mhaitsam18 <haitsam03@gmail.com>
2024-01-07 14:39:56 +0700

    fix

--- resources/views/admin/paket/create.blade.php
+++ resources/views/admin/paket/create.blade.php
@@ -71,6 +71,18 @@
                                     @enderror
                                 </div>
                                 <div class="mb-3">
+                                    <label for="fasilitas" class="form-label">Fasilitas</label>
+                                    <textarea class="form-control  @error('fasilitas') is-invalid @enderror" id="fasilitas" name="fasilitas"
+                                        placeholder="Fasilitas">{{ old('fasilitas') }}</textarea>
+                                    @error('fasilitas')
+                                        <div class="text-danger fs-6">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="mb-3">
                                     <label for="durasi" class="form-label">Durasi (hari)</label>
                                     <input type="number" class="form-control  @error('durasi') is-invalid @enderror"
                                         id="durasi" name="durasi" value="{{ old('durasi') }}" placeholder="Durasi">
@@ -85,16 +97,6 @@
                                     <input type="number" class="form-control  @error('harga') is-invalid @enderror"
                                         id="harga" name="harga" value="{{ old('harga') }}" placeholder="harga">
                                     @error('Harga')
-                                        <div class="text-danger fs-6">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
-                                <div class="mb-3">
-                                    <label for="fasilitas" class="form-label">Fasilitas</label>
-                                    <textarea class="form-control  @error('fasilitas') is-invalid @enderror" id="fasilitas" name="fasilitas"
-                                        placeholder="Fasilitas">{{ old('fasilitas') }}</textarea>
-                                    @error('fasilitas')
                                         <div class="text-danger fs-6">
                                             {{ $message }}
                                         </div>
@@ -183,7 +185,39 @@
     </div> <!-- row -->
 @endsection
 @section('script')
+    <script src="/assets-nobleui/vendors/tinymce/tinymce.min.js"></script>
+    <script src="/assets-nobleui/vendors/simplemde/simplemde.min.js"></script>
+
+    <!-- Custom js for this page -->
+    <script src="/assets-nobleui/js/tinymce.js"></script>
+    <script src="/assets-nobleui/js/simplemde.js"></script>
+    <script src="/assets-nobleui/js/ace.js"></script>
+    <!-- End custom js for this page -->
     <script>
+        if ($("#fasilitas").length) {
+            tinymce.init({
+                selector: '#fasilitas',
+                height: 400,
+                default_text_color: 'red',
+                plugins: [
+                    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                    'searchreplace wordcount visualblocks visualchars code fullscreen',
+                ],
+                toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
+                image_advtab: true,
+                templates: [{
+                        title: 'Test template 1',
+                        content: 'Test 1'
+                    },
+                    {
+                        title: 'Test template 2',
+                        content: 'Test 2'
+                    }
+                ],
+                content_css: []
+            });
+        }
         $(document).ready(function() {
             $('#gambar').on('change', function() {
                 const file = $(this).prop('files')[0];
