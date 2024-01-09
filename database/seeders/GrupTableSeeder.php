1b9f746f073782bee8713869a6eabd9ecf942af7
mhaitsam18 <haitsam03@gmail.com>
2024-01-09 17:00:36 +0700

    Update GrupTableSeeder.php

--- database/seeders/GrupTableSeeder.php
+++ database/seeders/GrupTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Grup;
+use App\Models\IsuPerjalanan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -32,6 +33,50 @@ class GrupTableSeeder extends Seeder
         ];
         foreach ($grups as $grup) {
             Grup::create($grup);
+        }
+
+        $isu_perjalanans = [
+            [
+                'grup_id' => 1,
+                'masalah' => 'Uji Test Fitur :)',
+                'solusi' => 'Stay Safe Semuaa ^_^',
+                'tanggal_pelaporan' => now(),
+                'tanggal_penyelesaian' => now(),
+                'status' => 1
+            ],
+            [
+                'grup_id' => 2,
+                'masalah' => 'Tidak Ada Masalah :)',
+                'solusi' => 'Stay Safe Semuaa ^_^',
+                'tanggal_pelaporan' => now(),
+                'tanggal_penyelesaian' => now(),
+                'status' => 1
+            ],
+        ];
+        foreach ($isu_perjalanans as $isu_perjalanan) {
+            IsuPerjalanan::create($isu_perjalanan);
+        }
+
+        $jadwals = [
+            [
+                'grup_id' => 1,
+                'nama_agenda' => 'Manasik Umroh',
+                'lokasi' => 'Kantor Pusat PT. Haifa Nida Wisata Karawang',
+                'waktu_mulai' => '2024-02-14 09:00:00',
+                'waktu_selesai' => '2024-02-14 15:00:00',
+                'keterangan' => 'Tidak Ada Keterangan',
+            ],
+            [
+                'grup_id' => 2,
+                'nama_agenda' => 'Manasik Umroh',
+                'lokasi' => 'Kantor Pusat PT. Haifa Nida Wisata Karawang',
+                'waktu_mulai' => '2024-02-14 09:00:00',
+                'waktu_selesai' => '2024-02-14 15:00:00',
+                'keterangan' => 'Tidak Ada Keterangan',
+            ],
+        ];
+        foreach ($jadwals as $jadwal) {
+            IsuPerjalanan::create($jadwal);
         }
     }
 }
