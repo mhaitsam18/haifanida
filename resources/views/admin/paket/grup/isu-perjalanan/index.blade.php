9c9c0305b4ce4d2e56f72d7e77890f90e86b4bf7
mhaitsam18 <haitsam03@gmail.com>
2024-01-11 16:21:42 +0700

    CRUD Isu Perjalanan Beres

--- resources/views/admin/paket/grup/isu-perjalanan/index.blade.php
+++ resources/views/admin/paket/grup/isu-perjalanan/index.blade.php
@@ -17,10 +17,8 @@
                     <div class="d-flex justify-content-between align-items-baseline mb-2">
                         <h6 class="card-title mb-2">{{ $title }}</h6>
                     </div>
-                    <a href="/admin/grup/{{ $grup->id }}/jadwal/create" class="btn btn-sm btn-langit mb-3"><i
-                            data-feather="plus" class="icon-sm me-2"></i> Tambah
-                        Data
-                        jadwal</a>
+                    <a href="/admin/grup/{{ $grup->id }}/isu-perjalanan/create" class="btn btn-sm btn-langit mb-3"><i
+                            data-feather="plus" class="icon-sm me-2"></i> Tambah Isu</a>
                     <a href="/admin/grup/{{ $grup->paket_id }}" class="btn btn-sm btn-secondary mb-3"><i
                             data-feather="arrow-left" class="icon-sm me-2"></i> Kembali</a>
                     <div class="table-responsive">
@@ -28,28 +26,29 @@
                             <thead>
                                 <tr>
                                     <th class="pt-0">#</th>
-                                    <th class="pt-0">Nama Agenda</th>
-                                    <th class="pt-0">Lokasi</th>
-                                    <th class="pt-0">Waktu Mulai</th>
-                                    <th class="pt-0">Waktu Selesai</th>
-                                    <th class="pt-0">Keterangan</th>
+                                    <th class="pt-0">Masalah</th>
+                                    <th class="pt-0">Solusi</th>
+                                    <th class="pt-0">Waktu Pelaporan</th>
+                                    <th class="pt-0">Waktu Penyelesaian</th>
+                                    <th class="pt-0">Status</th>
                                     <th class="pt-0">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($jadwals as $jadwal)
+                                @foreach ($isuPerjalanans as $isu)
                                     <tr>
                                         <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $jadwal->nama_agenda }}</td>
-                                        <td>{{ $jadwal->lokasi }}</td>
-                                        <td>{{ Carbon::parse($jadwal->waktu_mulai)->isoFormat('LLL') }}</td>
-                                        <td>{{ Carbon::parse($jadwal->waktu_selesai)->isoFormat('LLL') }}</td>
-                                        <td>{{ $jadwal->keterangan }}</td>
+                                        <td>{{ $isu->masalah }}</td>
+                                        <td>{{ $isu->solusi }}</td>
+                                        <td>{{ Carbon::parse($isu->waktu_pelaporan)->isoFormat('LLL') }}</td>
+                                        <td>{{ Carbon::parse($isu->waktu_penyelesaian)->isoFormat('LLL') }}
+                                        </td>
+                                        <td>{{ $isu->status ? 'Dalam Penanganan' : 'Sudah Selesai' }}</td>
                                         <td>
                                             <div class="d-flex align-items-center ">
-                                                <a href="/admin/jadwal/{{ $jadwal->id }}/edit"
+                                                <a href="/admin/isu-perjalanan/{{ $isu->id }}/edit"
                                                     class="badge bg-success d-inline-block ms-1">Edit</a>
-                                                <form action="/admin/jadwal/{{ $jadwal->id }}" method="post">
+                                                <form action="/admin/isu-perjalanan/{{ $isu->id }}" method="post">
                                                     @method('delete')
                                                     @csrf
                                                     <button type="submit"
