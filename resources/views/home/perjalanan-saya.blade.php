8764e3d08310c455085891fe58ed0c20a936ab34
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-05-19 15:40:09 +0700

    Menambah card history perjalanan di perjalanan-saya.blade.php

--- resources/views/home/perjalanan-saya.blade.php
+++ resources/views/home/perjalanan-saya.blade.php
@@ -1,11 +1,63 @@
 @extends('layouts.main')
 
 @section('content')
+
+    <div class="section-title text-center">
+        <h2>Perjalanan Saya</h2>
+    </div>
     
-    <div style="position: relative;">
-        <!-- TODO: load history perjalanan -->
-        <div class="content" style="min-height: 150vh;">
-            <!-- Page content that scrolls -->
+    <!-- TODO: load history perjalanan -->
+    <div class="container">
+        <div class="row justify-content-center my-5">
+            <div class="card col-md-6" style="width: 50rem">
+                <div class="card-body">
+                    <div class="card-header">
+                        <h3>NAMA PAKET</h3>
+                    </div>
+                    <div class="d-flex align-items-start m-3">
+                        <img src="{{ asset('storage/paket-gambar/umroh-24.jpg') }}" alt="Foto Paketnya di Sini" class="me-3"
+                        style="width: 200px; height: auto; border-radius: 8px;">
+                        <div class="container">
+                            <p>Jumlah Orang: 2 Orang</p>
+                            <p>Tanggal Keberangkatan: 31 Mei 2025</p>
+                            <p>Tanggal Kepulangan: 7 Juni 2025</p>
+
+                        </div>
+
+                    </div>
+                    <div class="d-flex bd-highlight justify-content-end align-items-center mt-3">
+                        <!-- Notifikasi status apabila terdapat berkas yang belum diverifikasi dan/atau tagihan yang belum lunas -->
+                        <p class="me-auto bd-highlight text-muted">Terdapat Berkas yang belum terverifikasi</p> 
+                        <a href="#" class="bd-highlight btn btn-primary">Verifikasi Berkas</a>
+                        <a href="#" class="bd-highlight btn btn-primary ms-2 disabled"">Tagihan Lunas</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card col-md-6 mt-3" style="width: 50rem">
+                <div class="card-body">
+                    <div class="card-header">
+                        <h3>NAMA PAKET</h3>
+                    </div>
+                    <div class="d-flex align-items-start m-3">
+                        <img src="{{ asset('storage/paket-gambar/umroh-24.jpg') }}" alt="Foto Paketnya di Sini" class="me-3"
+                        style="width: 200px; height: auto; border-radius: 8px;">
+                        <div class="container">
+                            <p>Jumlah Orang: 2 Orang</p>
+                            <p>Tanggal Keberangkatan: 31 Mei 2025</p>
+                            <p>Tanggal Kepulangan: 7 Juni 2025</p>
+
+                        </div>
+
+                    </div>
+                    <div class="d-flex bd-highlight justify-content-end align-items-center mt-3">
+                        <!-- Notifikasi status apabila terdapat berkas yang belum diverifikasi dan/atau tagihan yang belum lunas -->
+                        <p class="me-auto bd-highlight text-muted">Terdapat Berkas yang belum terverifikasi</p> 
+                        <a href="#" class="bd-highlight btn btn-primary">Verifikasi Berkas</a>
+                        <a href="#" class="bd-highlight btn btn-primary ms-2 disabled"">Tagihan Lunas</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
