a368ac67507e57aa60d5657af1f6ca6efb156705
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-05-20 16:50:00 +0700

    Import model dan menambah function 'tagihan' untuk page tagihan pada MemberController.php

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+// MODIFIED--
+use App\Models\Pemesanan;
+use App\Models\Pembayaran;
+// --MODIFIED
 
 class MemberController extends Controller
 {
@@ -25,10 +29,22 @@ class MemberController extends Controller
     }
 
     // MODIFIED--
+    // fungsi untuk view perjalanan saya
     public function perjalananSaya(Request $request){
         $user = Auth::user();
         $title = 'Perjalanan Saya | Haifa Nida Wisata';
         return view('home.perjalanan-saya', compact('user', 'title'));
+    }
+    // --MODIFIED
+
+    // MODIFIED--
+    public function tagihan(Request $request){
+        $user = Auth::user();
+        $title = 'Tagihan | Haifa Nida Wisata';
+
+        $tagihan = $user->pemesanans()->with('pembayarans.pemesanan.paket')->get()->flatMap->pembayarans;
+
+        return view('home.tagihan', compact('user', 'title', 'tagihan'));
     }
     // --MODIFIED
 }
