8ef80e5d989c52b846794feed341e09e24b3cad4
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-05-22 02:51:27 +0700

    penambahan hidden item user id

--- resources/views/home/pemesanan/pemesanan-umroh.blade.php
+++ resources/views/home/pemesanan/pemesanan-umroh.blade.php
@@ -29,14 +29,14 @@
                 <form method="POST" action="{{ route('pemesanan.store') }}" id="formPemesanan">
                     @csrf
                     <input type="hidden" name="paket_id" value="{{ $paket->id }}">
-
+                    <input type="hidden" name="user_id" value="{{ $user->id }}">
                     <!-- Form Jemaah -->
                     <div class="card mb-4">
                         <div class="card-header bg-light">
                             <h5 class="m-0"><i class='bx bx-user-circle'></i> Data Pemesan</h5>
                         </div>
                         <div class="card-body">
-                            <div class="mb-3">
+                            {{-- <div class="mb-3">
                                 <label for="nama_lengkap" class="form-label">Nama Lengkap <span class="text-danger">*</span></label>
                                 <input type="text" class="form-control @error('nama_lengkap') is-invalid @enderror" id="nama_lengkap" name="nama_lengkap" value="{{ old('nama_lengkap') }}" required>
                                 @error('nama_lengkap')
@@ -49,13 +49,16 @@
                                     <option value="" selected disabled>Pilih Metode Pembayaran</option>
                                     <option value="Transfer Bank" @selected(old('metode_pembayaran') == 'Transfer Bank')>Transfer Bank</option>
                                     <option value="Kartu Kredit" @selected(old('metode_pembayaran') == 'Kartu Kredit')>Kartu Kredit</option>
-                                    <option value="Cicilan" @selected(old('metode_pembayaran') == 'Cicilan')>Cicilan</option>
+                                    <option value="Cash" @selected(old('metode_pembayaran') == 'Cash')>Cash</option>
                                 </select>
                                 @error('metode_pembayaran')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
-                            </div>
+                            </div> --}}
                             <div class="text-end">
+                                <a href="/paket/{{ $paket->id }}"
+                                                class="btn btn-sm btn-secondary mb-1"><i data-feather="arrow-left"
+                                                    class="icon-sm me-2"></i>Kembali</a>
                                 <button type="submit" class="btn default-btn btn-bg-two border-radius-5">
                                     <i class='bx bx-check-circle'></i> Lanjutkan
                                 </button>
