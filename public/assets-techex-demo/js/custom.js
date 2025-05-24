ed7c30a5a7564b9f7812de7e3b1ecbfd0506f79d
Ibnu Fadhilah <ibnu.fadhil@upi.edu>
2025-05-25 01:43:16 +0700

    menonaktifkan pemanggilan nice-select karena menyebabkan konflik pada halaman selain admin(nobleUI)

--- public/assets-techex-demo/js/custom.js
+++ public/assets-techex-demo/js/custom.js
@@ -156,19 +156,21 @@
     });
 
     // Nice Select JS
-    $('select').niceSelect();
+    // MODIFIED--
+    // $('select').niceSelect();
 
-    // FAQ Accordion JS
-	$('.accordion').find('.accordion-title').on('click', function(){
-		// Adds Active Class
-		$(this).toggleClass('active');
-		// Expand or Collapse This Panel
-		$(this).next().slideToggle('fast');
-		// Hide The Other Panels
-		$('.accordion-content').not($(this).next()).slideUp('fast');
-		// Removes Active Class From Other Titles
-		$('.accordion-title').not($(this)).removeClass('active');
-    });
+    // // FAQ Accordion JS
+	// $('.accordion').find('.accordion-title').on('click', function(){
+	// 	// Adds Active Class
+	// 	$(this).toggleClass('active');
+	// 	// Expand or Collapse This Panel
+	// 	$(this).next().slideToggle('fast');
+	// 	// Hide The Other Panels
+	// 	$('.accordion-content').not($(this).next()).slideUp('fast');
+	// 	// Removes Active Class From Other Titles
+	// 	$('.accordion-title').not($(this)).removeClass('active');
+    // });
+    // --MODIFIED
 
     // Skill-bar JS
     $(".skill-bar").each(function () {
